e1b7de9bb03ede26978f74f31bbe7702be813979
swapnil solanki <ssinghsolanki686@gmail.com>
2023-07-06 14:09:45 +0530

    asdasd

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -69,6 +69,57 @@ bookRoute.get("/sort/:sort",async (req,res)=>{
 })
 
 
+bookRoute.get("/sort/:sort",async (req,res)=>{
+    const data = req.params.sort;
+    console.log(data)
+    try {
+        if(data=="lower"){
+        var newData =await BookModel.find().sort({Price:1})
+        }else{
+            var newData =await BookModel.find().sort({Price:-1})
+        }
+        res.json(newData)
+    } catch (error) {
+        console.log(error)
+        res.json("Something went wrong on delete route")
+    }
+})
+
+bookRoute.get("/both",async(req,res)=>{
+    var {genreFilter,sortOrder} = req.query
+    var Genre = genreFilter
+    var price  = sortOrder
+    try {
+        var data = []
+        console.log(Genre,price)
+        if(price=="asc" && Genre){
+            console.log("ssd")
+            var data = await BookModel.find({Genre}).sort({Price:1})
+        }
+        else if(price=="desc" && Genre){
+            var data = await BookModel.find({Genre}).sort({Price:-1})
+        }
+        else if(price=="desc" && Genre==undefined){
+            var data = await BookModel.find().sort({Price:-1})
+        }
+        else if(price=="asc" && Genre==undefined ){
+            var data = await BookModel.find({Genre}).sort({Price:1})
+        }
+        else if(price==undefined &&Genre ){
+            console.log(Genre)
+            var data = await BookModel.find({Genre})
+        }
+        else{
+            var data = await BookModel.find()
+        }res.json(data)
+        
+    } catch (error) {
+        console.log(error)
+        res.json("error")
+    }
+})
+
+
 
 
 module.exports = {bookRoute}
